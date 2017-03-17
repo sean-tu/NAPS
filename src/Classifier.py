a1026ebeb608d6b276ab2623622dec9ee594766e
Sean Reedy <seanreedy239@gmail.com>
2017-03-17 02:08:55 -0400

    Created document/class/corpus structure in Classification.py, added classifier processes in Classifier.py

--- src/Classifier.py
+++ src/Classifier.py
@@ -7,19 +7,44 @@ Out: class label
 
 __author__ = 'Sean Reedy'
 
+from nltk.classify import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
 class Classifier:
 
     def __init__(self):
         self.classifier = None
-        self.class_label = None
 
+    # TODO
     def load_classifier(self, path):
         """Choose classifier to use for classification from file"""
         pass
 
     def set_classifier(self, path=None):
-        self.classifier()
+        self.classifier = SklearnClassifier(MultinomialNB())
 
-    def classify(self):
+    def classify(self, document):
+        """Determine class probabilities of a given document
+
+        Decide conditional probability of classes for a document instance, using the classifier and corpus.
+        """
+        features = document.get_features()
+        probs = self.classifier.prob_classify(features)
+
+        # Select class with maximum probability
+        maxP = 0
+        label = None
+        for C, P in probs:
+            if P > maxP:
+                label = C
+        return label
+
+    def train(self, train_set):
+        self.classifier.train([(d.get_features(), d.get_label() ) for d in train_set])
+
+    # TODO
+    def test(self, test_set, train_set):
         pass
+
+
