04e49d1f76c3412e97629c9582b88d620c0cd1b8
vperidot <vjopro@gmail.com>
2017-04-09 21:58:14 -0400

    another media root & media url

--- NAPS/settings.py
+++ NAPS/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_ROOT = os.path.join(BASE_DIR, 'var/www/uploads/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 MEDIA_URL = 'http://projectnaps.me/uploads/'
